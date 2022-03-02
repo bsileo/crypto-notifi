c07b454b6c13c2bf55d39071343d551636367d0d
Brad Sileo <brad@sileo.name>
2022-03-02 09:52:17 -0500

    Add discord invite

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    <div class="container-fluid text-center bg-light p-2 fixed-bottom">
-      <p class="copyright">
-        Copyright 2022 DZ Assets, LLC |
-        <a href="mailto:info@cryptonotifi.xyz">info@cryptonotifi.xyz</a>
-      </p>
+    <div class="container-fluid bg-light p-2 fixed-bottom">
+      <div class="row">
+        <div class="flex xs4 copyright align--start">
+          Copyright 2022 DZ Assets, LLC |
+          <a href="mailto:info@cryptonotifi.xyz">info@cryptonotifi.xyz</a>
+        </div>
+        <div class="flex xs1 offset--xs7 align-content--end">
+          <a href="https://discord.gg/A7wQSH4f" target="_blank">
+            <va-image
+              contain
+              style="max-height: 16px"
+              src="/Discord-Logo-Black.svg"
+            />
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
